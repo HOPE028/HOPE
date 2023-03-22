88279d03d7c51f560853a9d6c566b13622bd2235
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-03-22 16:10:02 -0500

    Update to clarify that development is in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div className='App'>HOPE</div>
+  return (
+    <div className='App'>
+      <h1>HOPE</h1>
+      <h3>__In Progress__</h3>
+    </div>
+  )
 }
 
 export default App
